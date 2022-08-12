5131546b9cdfa2b15cd82aad8b42d17218d2d37f
Journey <ejy1105@gmail.com>
2022-08-12 15:37:31 +0900

    feat: 수정된 폴더 경로 import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Container from "../components/HomeStyle";
-import Nav from "../components/Nav";
-import Main from "../components/Main";
-import Paging from "@components/Paging";
+import Nav from "../components/nav/Nav";
+import Main from "../components/main/Main";
+import Paging from "@components/paging/Paging";
 import useSWR from "swr";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Size } from "interfaces/Project";
+import { Size, ProjectData } from "interfaces/Project";
 
 const Home: NextPage = () => {
   const [size, setSize] = useState<Size>();
-  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-  const { data, error } = useSWR("/api/projects", fetcher);
+  const fetcher = (url: string) =>
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((error) => {
+        error.response;
+      });
+  const { data } = useSWR("/api/projects", fetcher);
 
   const resizeHanlder = () => {
     const width = window.innerWidth;
@@ -22,6 +28,7 @@ const Home: NextPage = () => {
       height: height,
     });
   };
+
   useEffect(() => {
     setSize({
       width: window.innerWidth,
@@ -29,7 +36,6 @@ const Home: NextPage = () => {
     });
   }, []);
   useEffect(() => {
-    console.log("index resize useEffect");
     window.onresize = resizeHanlder;
   }, []);
   return (
